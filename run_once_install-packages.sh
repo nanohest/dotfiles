e2e3219ec65fd975860e2790959abd8f91d0dc99
Kristian Østergaard Martensen <kristian@efio.dk>
2022-05-25 09:22:31 +0200

    package management

---
+++ run_once_install-packages.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo dnf install --assumeyes \
+ https://dl.k6.io/rpm/repo.rpm \
+ fish \
+ starship \
+ kitty-doc kitty \
+ exa \
+ ripgrep \
+ snapd \
+ python3-pip.noarch \
+ pre-commit \
+ neovim \
+ jq \
+ gpgme \
+ podman-compose \
+ wl-clipboard \
+ dnf-plugins-core \
+ powertop \
+ bolt
+
+ # '@Development tools' cmake gcc-c++ libxcb-devel cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++
+ # brave-browser
+ # k6
