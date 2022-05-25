2f827b65e260d8dcabea7f98ffc529ce2891d53e
Kristian Østergaard Martensen <kristian@efio.dk>
2022-05-25 09:23:07 +0200

    not too sure...

---
+++ private_dot_local/private_share/chezmoi/literal_run_once_20-install-flatpack.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+flatpak install \
+com.getmailspring.Mailspring \
+com.github.tchx84.Flatseal \
+com.slack.Slack   \
+com.toggl.TogglDesktop \
+com.yubico.yubioath \
+io.atom.Atom \
+io.dbeaver.DBeaverCommunity \
+net.cozic.joplin_desktop \
+org.gabmus.whatip \
+org.gnome.DejaDup \
+org.gtk.Gtk3theme.Adwaita-dark \
+org.signal.Signal
+
