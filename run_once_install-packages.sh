53643714338f5c3ee3c15d481f2e99db90b70387
Kristian Østergaard Martensen <kristian@efio.dk>
2022-05-28 08:13:34 +0200

    install Brave browser

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
 sudo dnf install --assumeyes \
  https://dl.k6.io/rpm/repo.rpm \
  fish \
@@ -16,7 +20,8 @@ sudo dnf install --assumeyes \
  wl-clipboard \
  dnf-plugins-core \
  powertop \
- bolt
+ bolt \
+ brave-browser
 
  # '@Development tools' cmake gcc-c++ libxcb-devel cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++
  # brave-browser
