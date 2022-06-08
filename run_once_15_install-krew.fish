b994473723d41b37788900af14d77e89dd1e65c6
Kristian Østergaard Martensen <kristian@efio.dk>
2022-06-08 09:30:00 +0200

    install krew and kubectx+kubens

---
+++ run_once_15_install-krew.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/fish
+begin
+  set -x; set temp_dir (mktemp -d); cd "$temp_dir" &&
+  set OS (uname | tr '[:upper:]' '[:lower:]') &&
+  set ARCH (uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/') &&
+  set KREW krew-$OS"_"$ARCH &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/$KREW.tar.gz" &&
+  tar zxvf $KREW.tar.gz &&
+  ./$KREW install krew &&
+  set -e KREW; set -e temp_dir
+end
+
+kubectl krew install ctx ns
