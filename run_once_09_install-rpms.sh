f68cbf75fc029d2ebf96021e586bb85f12ed6d99
Kristian Østergaard Martensen <kristian@efio.dk>
2022-06-08 09:29:01 +0200

    suppress error when rpm is already installed (needs more work)

--- run_once_09_install-rpms.sh
+++ run_once_09_install-rpms.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Install displaylink driver. Reboot is required
-sudo rpm --install https://github.com/displaylink-rpm/displaylink-rpm/releases/download/v5.5.0/fedora-35-displaylink-1.10.1-1.x86_64.rpm
+sudo rpm --freshen https://github.com/displaylink-rpm/displaylink-rpm/releases/download/v5.5.0/fedora-35-displaylink-1.10.1-1.x86_64.rpm
 
