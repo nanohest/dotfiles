51491181b0e21980752c2d5efb93ede56e50e97a
Kristian Østergaard Martensen <kristian@efio.dk>
2022-06-09 08:31:15 +0200

    Update dot_config/private_fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,7 +3,9 @@ fish_add_path -m /var/lib/snapd/snap/bin
 fish_add_path -m ~/.krew/bin
 fish_add_path -m ~/.local/bin
 
-if [ -d ~/.tfenv/bin/ ]; fish_add_path -m ~/.tfenv/bin; end
+if test -d ~/.tfenv/bin/
+  fish_add_path -m ~/.tfenv/bin
+end
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/opt/google-cloud-sdk/path.fish.inc' ]; . '/opt/google-cloud-sdk/path.fish.inc'; end
